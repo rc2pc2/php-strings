746aa297aef5c398c7737ec80401aebec899de21
rc2pc2 <petriccarcc@gmail.com>
2023-07-03 09:45:16 +0200

    strings handling example in php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $stringa = 'Ciao amiche e amici';
+
+    $array = ['Michele', 'Fabio', 'Roberto'];
+
+    // echo $stringa . ' Daje!';
+
+    // var_dump($array);
+
+    // echo strlen($stringa);
+
+    // var_dump($stringa);
+
+    // echo ucwords($stringa);
+
+    $stringa = str_replace('e', '---', $stringa);
+
+    echo $stringa;
+
+
+?>
